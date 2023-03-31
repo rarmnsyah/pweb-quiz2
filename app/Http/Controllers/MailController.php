1e0665985745ad21ddfb3ce8d1fedc7fd8421e4d
Risky Armansyah <rarmnsyah787@gmail.com>
2023-03-31 22:01:45 +0700

    update and finishing send mail

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\DemoMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    public function index()
+    {
+        $mailData = [
+            'title' => 'Mail from ItSolutionStuff.com',
+            'body' => 'This is for testing email using smtp.'
+        ];
+         
+        Mail::to('advan0859@gmail.com')->send(new DemoMail($mailData));
+           
+        dd("Email is sent successfully.");
+    }
+}
